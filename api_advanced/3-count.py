10ddbf9a2ee24707d836b2e701e3576ad15d7026
Moussa-Kalam <m.amzat@alustudent.com>
2022-10-21 23:57:35 +0200

    Count it!

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,61 +1,58 @@
 #!/usr/bin/python3
-"""Parse the title of all hot articles,
-and print a sorted count of given keywords."""
 
-from audioop import reverse
+"""A recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords"""
+
+import json
 import requests
 
-headers = {'User-Agent': 'MyAPI/0.0.1'}
 
+def count_words(subreddit, word_list, after="", count=[]):
+    """Function to count_words
+    """
 
-def count_words(subreddit, word_list, after="", hot_list=[]):
+    if after == "":
+        count = [0] * len(word_list)
 
-    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
 
-    parameters = {'limit': 100, 'after': after}
-    response = requests.get(subreddit_url, headers=headers, params=parameters)
+    if request.status_code == 200:
+        data = request.json()
 
-    if response.status_code == 200:
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
 
-        json_data = response.json()
-        if (json_data.get('data').get('dist') == 0):
-            return
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
 
-        for child in json_data.get('data').get('children'):
-            title = child.get('data').get('title')
-            hot_list.append(title)
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
 
-        after = json_data.get('data').get('after')
-        if after is not None:
-
-            return count_words(subreddit, word_list,
-                               after=after, hot_list=hot_list)
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
-            counter = {}
-            for word in word_list:
-                word = word.lower()
-                if word not in counter.keys():
-                    counter[word] = 0
-                else:
-                    counter[word] += 1
-            for title in hot_list:
-                title_list = title.lower().split(' ')
-                for word in counter.keys():
-                    search_word = "{}".format(word)
-                    if search_word in title_list:
-                        counter[word] += 1
-            sorted_counter = dict(
-                sorted(counter.items(),
-                       key=lambda item: item[1], reverse=True))
-            for key, value in sorted_counter.items():
-                if value > 0:
-                    print("{}: {}".format(key, value))
-
-    else:
-        return
-
-
-if __name__ == '__main__':
-    count_words("hello", ['REDDIT', 'german', 'HI', 'whynot'])
-    count_words('unpopular', ['down', 'vote', 'downvote',
-                              'you', 'her', 'unpopular', 'politics'])
+            count_words(subreddit, word_list, after, count)
